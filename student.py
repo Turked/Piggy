50cfd7a1bec130ba9f8013e04b9d07d20704e1be
Edward Turk <91563267+Turked@users.noreply.github.com>
2021-11-02 19:12:37 +0000

    fail

--- student.py
+++ student.py
@@ -59,7 +59,9 @@ class Piggy(PiggyParent):
     def edward(self):
       for i in range(4):
         self.deg_fwd(180)
+        self.stop()
         self.right()
+        self.stop()
 
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
