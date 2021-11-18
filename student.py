0c23577f191b9a701a0ef0d3838c06ed6640ce71
Edward Turk <91563267+Turked@users.noreply.github.com>
2021-11-18 19:56:05 +0000

    Extra work

--- student.py
+++ student.py
@@ -148,17 +148,22 @@ class Piggy(PiggyParent):
             self.servo(1400)                          #Looking straight]
             time.sleep(1)                             #Looking straight]
             self.stop()                               #Looking straight]
-            if (right > left):                        #Is the right side shorter (Yes)
-              self.servo(1400)                        #Looking straight]
-              time.sleep(1)                           #Looking straight]
-              self.stop()                             #Looking straight]
-              self.wall_avoid()                       #Running Wall Avoid
-            elif (left > right):                      #Is the left side shorter (Yes)
-              self.servo(1400)                        #Looking straight]
-              time.sleep(1)                           #Looking straight]
-              self.stop()                             #Looking straight]
-              self.wall_avoid_left()                  #Running Wall Avoid Left
-              #Credit -> Vincent
+            if (right - left > 100):
+              if (right > left):                        #Is the right side shorter (Yes)
+                self.servo(1400)                        #Looking straight]
+                time.sleep(1)                           #Looking straight]
+                self.stop()                             #Looking straight]
+                self.wall_avoid()                       #Running Wall Avoid
+              elif (left > right):                      #Is the left side shorter (Yes)
+                self.servo(1400)                        #Looking straight]
+                time.sleep(1)                           #Looking straight]
+                self.stop()                             #Looking straight]
+                self.wall_avoid_left()                  #Running Wall Avoid Left
+            else:
+              self.back()
+              time.sleep(1)
+              self.stop()
+                #Credit -> Vincent
               
 
 
